1d621be0d77db663a2b11c7ddee561904e03f4a6
Ashik <ashikofficial455@gmail.com>
2024-02-22 22:11:50 +0530

    added events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -3,37 +3,61 @@ import "../styles/Events.css";
 
 function Events() {
   return (
-    <div className="Events__container mt-7 flex flex-col items-center justify-center gap-3 text-center">
-      <h1 className=" text-5xl">In Frames</h1>
-      <h2 className=" text-2xl my-3">Technical Events</h2>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Debate</h3>
+    <div className="events__container flex flex-col items-center justify-center text-center">
+      <h1 className="in__frames text-5xl">
+        <strong>In Frames</strong>
+      </h1>
+      <h2 className="h2__child text-2xl">Technical Events</h2>
+      <div className="flex__pairs">
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Debate</h3>
+          </div>
+        </div>
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Idea Presentation</h3>
+          </div>
+        </div>
       </div>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Idea Presentation</h3>
+      <div className="flex__pairs">
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Extempore Speech</h3>
+          </div>
+        </div>
       </div>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Extempore Speech</h3>
+      <h2 className="h2__child text-3xl">Non - Technical Events</h2>
+      <div className="flex__pairs">
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Mind Maze</h3>
+          </div>
+        </div>
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Poster Making</h3>
+          </div>
+        </div>
       </div>
-      <h2 className="text-3xl my-3">Non - Technical Events</h2>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Mind Maze</h3>
-      </div>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Poster Making</h3>
-      </div>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Treasure Hunt</h3>
-      </div>
-      <div className="grid__containers">
-        <img src="./logo.jpeg" alt="logo-for-" className="w-80"/>
-        <h3 className="mb-2 text-xl">Puzzle Competition</h3>
+      <div className="flex__pairs">
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Treasure Hunt</h3>
+          </div>
+        </div>
+        <div className="grid__containers">
+          <img src="./logo.jpeg" alt="logo-for-" className="w-80" />
+          <div className="h3__container">
+            <h3 className="mb-2 text-xl">Puzzle Competition</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,13 +12,13 @@ const Hero = () => {
       <h2 className="hero__subtitle mt-2 text-base sm:mt-0 sm:text-lg xl:text-xl">
         <strong className="text-slate-300">We, Welcome You To,</strong>
       </h2>
-      <h1 className="hero__title text-7xl sm:text-8xl xl:text-9xl">
+      <h1 className="hero__title text-7xl sm:text-8xl xl:mt-1 xl:text-[8rem]">
         <strong className="ash__name">ASH</strong>
-        <span className="text-7xl font-black text-indigo-600 sm:text-8xl xl:text-9xl">
+        <span className="text-7xl font-black text-indigo-600 sm:text-8xl xl:text-[8rem]">
           SYM
         </span>
       </h1>
-      <p className="hero__discription leading-1 mt-1 max-w-[50ch] text-center text-sm font-medium text-slate-500 sm:text-base">
+      <p className="hero__discription leading-1 mt-1 max-w-[50ch] text-center text-sm font-medium text-slate-500 sm:text-base xl:mt-3">
         We, Students and faculty of <strong>Science and Humanities</strong>{" "}
         Department, are very grateful to invite you to{" "}
         <strong className="text-indigo-600">AshSym!</strong>, a technical fest
@@ -29,7 +29,9 @@ const Hero = () => {
         </u>{" "}
         below.
       </p>
-      <button className="hero__btn btn mt-4 sm:text-base">Get Started</button>
+      <button className="hero__btn btn mt-4 sm:text-base xl:mt-6">
+        Get Started
+      </button>
       <hr className="hr__each" />
     </section>
   );
