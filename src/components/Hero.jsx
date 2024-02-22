e2ee8f2bdbc262462b806d2352feca6ba0e95ced
Ashik <ashikofficial455@gmail.com>
2024-02-22 19:33:15 +0530

    minor fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../styles/Hero.css";
 
 const Hero = () => {
   return (
-    <section className="hero align-center md: container flex flex-col items-center justify-center md:mt-10">
+    <section className="hero align-center md: container flex flex-col items-center justify-center md:mt-14">
       <img
         src="./logo.jpeg"
         alt="hello"
@@ -13,12 +13,12 @@ const Hero = () => {
         <strong className="text-slate-300">We, Welcome You To,</strong>
       </h2>
       <h1 className="hero__title text-7xl sm:text-8xl xl:text-9xl">
-        <strong className=" opacity-80">ASH</strong>
+        <strong className="ash__name">ASH</strong>
         <span className="text-7xl font-black text-indigo-600 sm:text-8xl xl:text-9xl">
           SYM
         </span>
       </h1>
-      <p className="hero__discription leading-1 mt-1 max-w-[50ch] text-center text-sm font-medium text-slate-500 sm:text-base">
+      <p className="hero__discription leading-1 mt-1 max-w-[50ch] text-center text-sm font-medium text-slate-500 sm:text-base md:mt-2">
         We, Students and faculty of <strong>Science and Humanities</strong>{" "}
         Department, are very grateful to invite you to{" "}
         <strong className="text-indigo-600">AshSym!</strong>, a technical fest
@@ -29,8 +29,8 @@ const Hero = () => {
         </u>{" "}
         below.
       </p>
-      <button className="hero__btn btn mt-4 sm:text-base">Get Started</button>
-      <hr className="hr__each " />
+      <button className="hero__btn btn mt-4 sm:text-base md:mt-7">Get Started</button>
+      <hr className="hr__each" />
     </section>
   );
 };
