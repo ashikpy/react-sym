8aebb2e21466169faeef2bf8971cc29f51283999
Ashik <ashikofficial455@gmail.com>
2024-03-02 19:31:05 +0530

    added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,24 @@ import "../styles/About.css";
 
 function About() {
   return (
-    <div
-      className="about__container item-center flex flex-col justify-center text-center"
-      id="about"
-    >
+    <div className="about__container" id="about">
       <h1>
-        <strong className="about__text text-5xl">About</strong>
+        <strong className="about__text text-5xl">Venue</strong>
       </h1>
-      <p className="h1__child text-2xl">Venue</p>
+
+      <p className="h1__child m-auto mt-[1.5rem] max-w-[24ch] text-2xl">
+        Amrita College of Enginerring And Technology
+      </p>
+      <div className="parent__grid">
+        <div className="grid__containers__about"></div>
+        <div className="grid__containers__about"></div>
+        <div className="grid__containers__about">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d789.9493377737289!2d77.41363662688725!3d8.230374629726596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f12c00000001%3A0x8c485f52f39feebf!2sAmrita%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1709387570853!5m2!1sen!2sin"
+            className="map__iframe"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
