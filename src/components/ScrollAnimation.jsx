eb0a03e41d37cc8891ac3a85282cc2b9e3313ed7
Ashik <ashikofficial455@gmail.com>
2024-03-04 20:24:35 +0530

    optimised

--- src/components/ScrollAnimation.jsx
+++ src/components/ScrollAnimation.jsx
@@ -1,135 +1,43 @@
 import "../styles/ScrollAnimation.css";
 
 function ScrollAnimation() {
+  const revealClasses = [
+    {
+      className: ".reveal-events",
+      revealPoint: 50,
+      activeClass: "active-events",
+    },
+    { className: ".reveal", revealPoint: 50, activeClass: "active" },
+    { className: ".reveal-2", revealPoint: -10, activeClass: "active-2" },
+    { className: ".reveal-3", revealPoint: -10, activeClass: "active-3" },
+    { className: ".reveal-4", revealPoint: -10, activeClass: "active-4" },
+    { className: ".reveal-5", revealPoint: -10, activeClass: "active-5" },
+    { className: ".reveal-7", revealPoint: -10, activeClass: "active-7" },
+    { className: ".reveal-8", revealPoint: -10, activeClass: "active-8" },
+    { className: ".reveal-9", revealPoint: -10, activeClass: "active-9" },
+    { className: ".reveal-10", revealPoint: -10, activeClass: "active-10" },
+  ];
+
   window.addEventListener("scroll", function () {
-    let revealsEvents = document.querySelectorAll(".reveal-events");
+    let windowHeight = window.innerHeight;
     let scrollDownSvg = document.querySelector(".Scroll__container");
-    for (let i = 0; i < revealsEvents.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = revealsEvents[i].getBoundingClientRect().top;
-      let revealPoint = 50;
-      if (revealTop < windowHeight - revealPoint) {
-        revealsEvents[i].classList.add("active-events");
-        scrollDownSvg.classList.add("Scroll__container__hide");
+
+    revealClasses.forEach(({ className, revealPoint, activeClass }) => {
+      let elements = document.querySelectorAll(className);
+
+      for (let i = 0; i < elements.length; i++) {
+        let revealTop = elements[i].getBoundingClientRect().top;
+
+        if (revealTop < windowHeight - revealPoint) {
+          elements[i].classList.add(activeClass);
+          if (className === ".reveal-events") {
+            scrollDownSvg.classList.add("Scroll__container__hide");
+          }
+        } else {
+          elements[i].classList.remove(activeClass);
+        }
       }
-    }
-
-    let reveals = document.querySelectorAll(".reveal");
-    for (let i = 0; i < reveals.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals[i].getBoundingClientRect().top;
-      let revealPoint = 50;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-
-    let reveals2 = document.querySelectorAll(".reveal-2");
-    for (let i = 0; i < reveals2.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals2[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals2[i].classList.add("active-2");
-      } else {
-        reveals2[i].classList.remove("active-2");
-      }
-    }
-
-    let reveals3 = document.querySelectorAll(".reveal-3");
-    for (let i = 0; i < reveals3.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals3[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals3[i].classList.add("active-3");
-      } else {
-        reveals3[i].classList.remove("active-3");
-      }
-    }
-
-    let reveals4 = document.querySelectorAll(".reveal-4");
-    for (let i = 0; i < reveals4.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals4[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals4[i].classList.add("active-4");
-      } else {
-        reveals4[i].classList.remove("active-4");
-      }
-    }
-
-    let reveals5 = document.querySelectorAll(".reveal-5");
-    for (let i = 0; i < reveals5.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals5[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals5[i].classList.add("active-5");
-      } else {
-        reveals5[i].classList.remove("active-5");
-      }
-    }
-
-    let reveals7 = document.querySelectorAll(".reveal-7");
-    for (let i = 0; i < reveals7.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals7[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals7[i].classList.add("active-7");
-      } else {
-        reveals7[i].classList.remove("active-7");
-      }
-    }
-
-    let reveals8 = document.querySelectorAll(".reveal-8");
-    for (let i = 0; i < reveals8.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals8[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals8[i].classList.add("active-8");
-      } else {
-        reveals8[i].classList.remove("active-8");
-      }
-    }
-
-    let reveals9 = document.querySelectorAll(".reveal-9");
-    for (let i = 0; i < reveals9.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals9[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals9[i].classList.add("active-9");
-      } else {
-        reveals9[i].classList.remove("active-9");
-      }
-    }
-
-    let reveals10 = document.querySelectorAll(".reveal-10");
-    for (let i = 0; i < reveals10.length; i++) {
-      let windowHeight = window.innerHeight;
-      let revealTop = reveals10[i].getBoundingClientRect().top;
-      let revealPoint = -10;
-
-      if (revealTop < windowHeight - revealPoint) {
-        reveals10[i].classList.add("active-10");
-      } else {
-        reveals10[i].classList.remove("active-10");
-      }
-    }
+    });
   });
 }
 
