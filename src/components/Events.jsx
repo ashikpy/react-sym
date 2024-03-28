3933716bc726c21d87f8f001f534ab0f29d391da
Ashik <ashikofficial455@gmail.com>
2024-03-28 16:09:08 +0530

    routes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -18,6 +18,34 @@ function Events() {
     }
   }
 
+  function handleDebateClick() {
+    window.open("/debate");
+  }
+
+  function handleIdeaClick() {
+    window.open("/idea");
+  }
+
+  function handleSpeech() {
+    window.open("/speech");
+  }
+
+  function handleMindMaze() {
+    window.open("/maze");
+  }
+
+  function handlePoster() {
+    window.open("/poster");
+  }
+
+  function handleTreasureHunt() {
+    window.open("/hunt");
+  }
+
+  function handlePuzzle() {
+    window.open("/puzzle");
+  }
+
   return (
     <div
       className="events__container reveal-events flex flex-col items-center justify-center text-center"
@@ -28,7 +56,7 @@ function Events() {
       </h1>
       <h2 className="h2__child reveal-2 text-2xl">Technical Events</h2>
       <div className="flex__pairs">
-        <div className="grid__containers reveal-7">
+        <div className="grid__containers reveal-7" onClick={handleDebateClick}>
           <img
             src="./Debate.png"
             alt="logo-for-debate"
@@ -38,7 +66,7 @@ function Events() {
             <h3 className="mb-2 text-xl">Debate</h3>
           </div>
         </div>
-        <div className="grid__containers reveal-7">
+        <div className="grid__containers reveal-7" onClick={handleIdeaClick}>
           <img src="./skill1.png" alt="logo-for-" className="w-80" />
           <div className="h3__container">
             <h3 className="mb-2 text-xl">Idea Presentation</h3>
@@ -46,26 +74,32 @@ function Events() {
         </div>
       </div>
       <div className="flex__single">
-        <div className="grid__containers grid__container__last reveal-10">
+        <div
+          className="grid__containers grid__container__last reveal-10"
+          onClick={handleSpeech}
+        >
           <img
             src={handleSwitch()}
             alt="logo-for-speech"
             className="logo__speech img__container__last w-80"
           />
-          <div className="h3__container h3__container__last">
+          <div
+            className="h3__container h3__container__last"
+            onClick={handleSpeech}
+          >
             <h3 className="mb-2 text-xl">Extempore Speech</h3>
           </div>
         </div>
       </div>
       <h2 className="h2__child reveal text-3xl">Non - Technical Events</h2>
       <div className="flex__pairs">
-        <div className="grid__containers reveal-8">
+        <div className="grid__containers reveal-8" onClick={handleMindMaze}>
           <img src="./mind_maze.png" alt="logo-for-" className="w-80" />
           <div className="h3__container">
             <h3 className="mb-2 text-xl">Mind Maze</h3>
           </div>
         </div>
-        <div className="grid__containers reveal-9">
+        <div className="grid__containers reveal-9" onClick={handlePoster}>
           <img src="./poster.png" alt="logo-for-" className="w-80" />
           <div className="h3__container">
             <h3 className="mb-2 text-xl">Poster Making</h3>
@@ -74,13 +108,23 @@ function Events() {
       </div>
       <div className="flex__pairs">
         <div className="grid__containers reveal-8">
-          <img src="./treasure_hunt.png" alt="logo-for-" className="w-80" />
+          <img
+            src="./treasure_hunt.png"
+            alt="logo-for-"
+            className="w-80"
+            onClick={handleTreasureHunt}
+          />
           <div className="h3__container">
             <h3 className="mb-2 text-xl">Treasure Hunt</h3>
           </div>
         </div>
         <div className="grid__containers reveal-9">
-          <img src="./Puzzle.png" alt="logo-for-" className="w-80" />
+          <img
+            src="./Puzzle.png"
+            alt="logo-for-"
+            className="w-80"
+            onClick={handlePuzzle}
+          />
           <div className="h3__container">
             <h3 className="mb-2 text-xl">Puzzle Competition</h3>
           </div>
