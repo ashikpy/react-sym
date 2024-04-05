2f4afe83ee63b1935b79fc4e5217d088c4200ecf
Ashik <ashikofficial455@gmail.com>
2024-04-05 12:44:57 +0530

    debate

--- src/pages/Debate.jsx
+++ src/pages/Debate.jsx
@@ -13,7 +13,67 @@ function Debate() {
           </h3>
         </header>
         <hr />
-        <section></section>
+        <section>
+          <ul>
+            <li>
+              <li>Group formation for debate will be two-on-two or 2V2.</li>
+              <li>
+                Participants from the same college can pair up and form their
+                own teams.
+              </li>
+              <li>
+                Topic will be given on the spot and each group will have 10
+                minutes to prepare on the topic.
+              </li>
+              <li>
+                Team members can use a paper to make small notes and can use it
+                while expressing their proposition.
+              </li>
+              <li>Facts must be accurate. He who asserts must prove. </li>
+              <li>
+                Each team will be given one turn for their constructive speech
+                and one turn for their rebuttal (Counter) speech with designated
+                speaking time.
+              </li>
+              <li>
+                The constructive speech can last for 4 minutes; whereas the time
+                allotted for counter session is 2 minutes.
+              </li>
+              <li>Keep track of time while taking each turn.</li>
+              <li>
+                Judges evaluate the quality of arguments, evidence and
+                presentation.
+              </li>
+              <li>
+                The team that scores the highest will be declared the winner and
+                the second highest scorer will be declared as the runner-up of
+                the debate.
+              </li>
+              <br />
+              <h1>Restrictions</h1>
+              <br />
+              <li>
+                Reading out the entire speech from a paper is not allowed.
+              </li>
+              <li>Mobile Phones, visual materials are denied.</li>
+              <li>Interrupting a speaker is strictly forbidden.</li>
+              <li>
+                No expression shall be accepted which is offensive personal or
+                defamatory in nature.
+              </li>
+              <br />
+              <h1>Point System</h1>
+              <br />
+              <li>
+                1. Relevance - speaking to the topic, using strong and
+                persuasive arguments.
+              </li>
+              <li>2. Fluency - speaking readily without pauses.</li>
+              <li>3. Engaging of all members in the debate.</li>
+              <li>4. Correct grammatical structures and Pronunciation.</li>
+            </li>
+          </ul>
+        </section>
       </div>
     </div>
   );
